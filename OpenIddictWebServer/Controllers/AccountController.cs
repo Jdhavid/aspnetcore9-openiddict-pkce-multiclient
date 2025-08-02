d4ac4846c6fead15eb0386c11ab484f4a652f57d
jdhavid <j.dhavid@gmail.com>
2025-08-01 20:48:27 -0500

    Crear controlador y vista para registro de usuario 1ra version

---
+++ OpenIddictWebServer/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenIddictWebServer.Models.ViewModels;
+
+namespace OpenIddictWebServer.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register(string returnUrl = null)
+        {
+            var registerViewModel = new RegisterViewModel();
+
+            ViewData["ReturnUrl"] = returnUrl;
+            return View(registerViewModel);
+        }
+
+
+    }
+}
